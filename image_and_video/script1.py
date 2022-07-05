bcd487260ee0980da52bdeecf062c12068acd57a
hdavidlp <66928964+hdavidlp@users.noreply.github.com>
2022-07-05 14:41:09 -0700

    manejo de imagenes OpenCV

---
+++ image_and_video/script1.py
@@ -0,0 +1,20 @@
+import cv2
+
+img = cv2.imread("galaxy.jpg", 0)
+
+print(type(img))
+print(img)
+print(img.shape)
+print(img.ndim)
+
+resized_image = cv2.resize(img, (int(img.shape[1]/2),int(img.shape[0]/2)))
+#cv2.imshow("galaxy", img)
+
+cv2.imshow("galaxy", resized_image)
+
+cv2.imwrite("Galaxy_resized.jpg", resized_image)
+
+
+cv2.waitKey(2000)
+cv2.destroyAllWindows()
+
